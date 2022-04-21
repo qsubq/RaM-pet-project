2748dcf7454641a9e4caedf1d9f528457a4c77c3
FataLity2328 <maksim-kargin@bk.ru>
2022-04-21 10:20:02 +0300

    added tool bar and navigation drawer. deleted splash screen

--- app/src/main/java/com/github/qsubq/rampetproject/MainActivity.kt
+++ app/src/main/java/com/github/qsubq/rampetproject/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.github.qsubq.rampetproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.github.qsubq.rampetproject.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     lateinit var navController: NavController
+    private lateinit var appBarConfig: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +26,22 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_fragment) as NavHostFragment
         navController = navHostFragment.navController
+
+        binding.navView.setupWithNavController(navController)
+
+        setSupportActionBar(toolbar)
+        appBarConfig = AppBarConfiguration(navController.graph, drawer_layout)
+        setupActionBarWithNavController(navController, appBarConfig)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
+    }
+
+
 }
