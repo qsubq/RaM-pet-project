1e802abf30ae78a7f02778e243439d6953c6e6e2
FataLity2328 <maksim-kargin@bk.ru>
2022-10-03 15:31:24 +0300

    Bug fixed

--- app/src/main/java/com/github/qsubq/rampetproject/app/presentation/screen/character/RandomCharacterFragment.kt
+++ app/src/main/java/com/github/qsubq/rampetproject/app/presentation/screen/character/RandomCharacterFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.view.isEmpty
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -33,7 +32,7 @@ class RandomCharacterFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         init()
 
-        if (binding.rvCharacters.isEmpty()) {
+        if (!viewModel.isAlreadyGetCharacters) {
             viewModel.getRandomCharacters()
         }
     }
--- app/src/main/java/com/github/qsubq/rampetproject/app/presentation/screen/character/RandomCharacterViewModel.kt
+++ app/src/main/java/com/github/qsubq/rampetproject/app/presentation/screen/character/RandomCharacterViewModel.kt
@@ -3,7 +3,6 @@ package com.github.qsubq.rampetproject.app.presentation.screen.character
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.qsubq.rampetproject.data.model.characterModel.CharacterModel
-import com.github.qsubq.rampetproject.data.repository.RemoteRepositoryImpl
 import com.github.qsubq.rampetproject.data.repository.RemoteRepositoryImpl.*
 import com.github.qsubq.rampetproject.domain.useCase.GetRandomCharacterUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,13 +15,15 @@ import javax.inject.Inject
 class RandomCharacterViewModel @Inject constructor(
     private val useCase: GetRandomCharacterUseCase,
 ) : ViewModel() {
-    private val _myUiState = MutableStateFlow<NetworkResult<CharacterModel>>(NetworkResult.Success<CharacterModel>(
+    private val _myUiState = MutableStateFlow<NetworkResult<CharacterModel>>(NetworkResult.Success(
         CharacterModel()))
     val myUiState: StateFlow<NetworkResult<CharacterModel>> = _myUiState
+    var isAlreadyGetCharacters: Boolean = false
 
     fun getRandomCharacters() {
         viewModelScope.launch {
             _myUiState.value = useCase.execute()
         }
+        isAlreadyGetCharacters = true
     }
 }
